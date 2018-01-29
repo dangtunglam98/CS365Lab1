0582989dbab64dadc4d88d8f0d0ae312d0ab8e37
Lam Dang <tunglam2051998@gmail.com>
2018-01-28 21:09:27 -0500

    maze2graph update

--- mazeClass.py
+++ mazeClass.py
@@ -33,43 +33,51 @@ class Maze:
 			for i in range(self.rowsNum-1):
 				if self.maze[i][j] != "%":
 					graph.update({(i,j):[]})
-		
+		for row , col in graph.keys():
+			if row < self.rowsNum - 1 and self.maze[row+1][col] != "%":
+				graph[(row,col)].append(("S",(row+1,col)))
+				graph[(row+1),col].append(("N",(row,col)))
+			if col < self.colsNum - 1 and self.maze[row][col+1] != "%":
+				graph[(row,col)].append(("E",(row,col+1)))
+				graph[(row,col+1)].append(("W",(row,col)))
 		return graph
 
-	def move(self,direction):
-		xCor = self.currentXcor
-		yCor = self.currentYcor
-		currentPos = (xCor,yCor)
-		if direction == "up":
-			if self.maze[xCor - 1][yCor] == "%":
-				return ("Blocked")
-			else:
-				xCor = xCor - 1
-				currentPos = (xCor, yCor)
-		elif direction == "down":
-			if self.maze[xCor + 1][yCor] == "%":
-				return "Blocked"
-			else:
-				xCor = xCor + 1
-				currentPos = (xCor, yCor)
-		if direction == "left":
-			if self.maze[xCor][yCor - 1] == "%":
-				return ("Blocked")
-			else:
-				yCor = yCor - 1
-				currentPos = (xCor, yCor)
-		if direction == "right":
-			if self.maze[xCor][yCor + 1] == "%":
-				return ("Blocked")
-			else:
-				yCor = yCor + 1
-				currentPos = (xCor, yCor)
-		self.currentXcor = xCor
-		self.currentYcor = yCor
-		return currentPos
+
+	# def move(self,direction):
+	# 	xCor = self.currentXcor
+	# 	yCor = self.currentYcor
+	# 	currentPos = (xCor,yCor)
+	# 	if direction == "up":
+	# 		if self.maze[xCor - 1][yCor] == "%":
+	# 			return ("Blocked")
+	# 		else:
+	# 			xCor = xCor - 1
+	# 			currentPos = (xCor, yCor)
+	# 	elif direction == "down":
+	# 		if self.maze[xCor + 1][yCor] == "%":
+	# 			return "Blocked"
+	# 		else:
+	# 			xCor = xCor + 1
+	# 			currentPos = (xCor, yCor)
+	# 	if direction == "left":
+	# 		if self.maze[xCor][yCor - 1] == "%":
+	# 			return ("Blocked")
+	# 		else:
+	# 			yCor = yCor - 1
+	# 			currentPos = (xCor, yCor)
+	# 	if direction == "right":
+	# 		if self.maze[xCor][yCor + 1] == "%":
+	# 			return ("Blocked")
+	# 		else:
+	# 			yCor = yCor + 1
+	# 			currentPos = (xCor, yCor)
+	# 	self.currentXcor = xCor
+	# 	self.currentYcor = yCor
+	# 	return currentPos
 
 maze = Maze("1prize-open.txt")
 print(maze.maze2graph())
-
+print(maze.startRow)
+print(maze.startCol)
 
 
