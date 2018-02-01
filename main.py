9caeb3fa09cbaba0df374c31bc19f5e7eb396aca
Anh Dang <aqdang16@earlham.edu>
2018-01-31 22:36:25 -0500

    edit main

--- main.py
+++ main.py
@@ -1,15 +1,82 @@
 from singlePrize import single_bfs, single_dfs, single_gbfs, single_astar
 from multiPrize import multi_astar
 
-one_prize_file = ["1prize-open.txt", "1prize-medium.txt", "1prize-large.txt"]
-multi_prize_file = ["multiprize-tiny.txt","multiprize-small.txt","multiprize-medium.txt"]
+def main():
 
-for file in one_prize_file:
-    single_bfs(file)
-    single_dfs(file)
-    single_gbfs(file)
-    single_astar(file)
+    print("\nChoose a maze file:")
+    print("1. 1prize-open.txt")
+    print("2. 1prize-medium.txt")
+    print("3. 1prize-large.txt")
+    print("4. multiprize-tiny.txt")
+    print("5. multiprize-small.txt")
+    print("6. multiprize-medium.txt\n")
+    firstinput = input("Enter the number to select a file: ")
 
-for file in multi_prize_file:
-    multi_astar(file)
+    if firstinput == "1":
+        print("\nChoose a search algorithm:")
+        print("1. Breadth-first search")
+        print("2. Depth-first search")
+        print("3. Greedy best-first search")
+        print("4. A* search.\n")
+        secondinput = input("Enter the number to select an algorithm: ")
+        if secondinput == "1":
+            single_bfs("1prize-open.txt")
+        elif secondinput == "2":
+            single_dfs("1prize-open.txt")
+        elif secondinput == "3":
+            single_gbfs("1prize-open.txt")
+        elif secondinput == "4":
+            single_astar("1prize-open.txt")
+        else:
+            print("Not applicable input")
 
+    elif firstinput == "2":
+        print("\nChoose a search algorithm:")
+        print("1. Breadth-first search")
+        print("2. Depth-first search")
+        print("3. Greedy best-first search")
+        print("4. A* search.\n")
+        secondinput = input("Enter the number to select an algorithm: ")
+        if secondinput == "1":
+            single_bfs("1prize-medium.txt")
+        elif secondinput == "2":
+            single_dfs("1prize-medium.txt")
+        elif secondinput == "3":
+            single_gbfs("1prize-medium.txt")
+        elif secondinput == "4":
+            single_astar("1prize-medium.txt")
+        else:
+            print("Not applicable input")
+
+    elif firstinput == "3":
+        print("\nChoose a search algorithm:")
+        print("1. Breadth-first search")
+        print("2. Depth-first search")
+        print("3. Greedy best-first search")
+        print("4. A* search.\n")
+        secondinput = input("Enter the number to select an algorithm: ")
+        if secondinput == "1":
+            single_bfs("1prize-large.txt")
+        elif secondinput == "2":
+            single_dfs("1prize-large.txt")
+        elif secondinput == "3":
+            single_gbfs("1prize-large.txt")
+        elif secondinput == "4":
+            single_astar("1prize-large.txt")
+        else:
+            print("Not applicable input")
+
+    elif firstinput == "4":
+        multi_astar("multiprize-tiny.txt")
+
+    elif firstinput == "5":
+        multi_astar("multiprize-small.txt")
+
+    elif firstinput == "6":
+        multi_astar("multiprize-medium.txt")
+
+    else:
+        print("Not applicable input")
+
+if __name__ == "__main__":
+    main()
